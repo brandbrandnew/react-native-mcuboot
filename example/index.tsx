168f0fdd4692fda021f755fe8b05a6a12c2a49ab
Sparsh Tyagi <sparshtyagi21@gmail.com>
2022-03-10 10:52:01 +0100

    add react-native-fs

--- example/index.tsx
+++ example/index.tsx
@@ -1,5 +1,6 @@
-import { AppRegistry } from 'react-native';
+import { AppRegistry, LogBox } from 'react-native';
 import App from './src/App';
 import { name as appName } from './app.json';
 
+LogBox.ignoreLogs(['new NativeEventEmitter']);
 AppRegistry.registerComponent(appName, () => App);

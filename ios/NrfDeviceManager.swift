9e2e503096bd0dc48dfb99667b5aa1d003b12956
Sparsh Tyagi <sparshtyagi21@gmail.com>
2022-03-11 12:01:37 +0100

    remove more ios nrf old references

--- ios/NrfDeviceManager.swift
+++
@@ -1,8 +0,0 @@
-@objc(Mcuboot)
-class Mcuboot: NSObject {
-
-    @objc(multiply:withB:withResolver:withRejecter:)
-    func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-        resolve(a*b)
-    }
-}

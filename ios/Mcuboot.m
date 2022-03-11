90658c24eb96255522ff89d5c4848f039a2b9558
Sparsh Tyagi <sparshtyagi21@gmail.com>
2022-03-11 12:02:20 +0100

    add methods to bridge

--- ios/Mcuboot.m
+++ ios/Mcuboot.m
@@ -1,9 +1,21 @@
 #import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>
 
-@interface RCT_EXTERN_MODULE(Mcuboot, NSObject)
+@interface RCT_EXTERN_REMAP_MODULE(Mcuboot, RNMcuboot, RCTEventEmitter)
 
-RCT_EXTERN_METHOD(multiply:(float)a withB:(float)b
-                 withResolver:(RCTPromiseResolveBlock)resolve
-                 withRejecter:(RCTPromiseRejectBlock)reject)
++ (BOOL)requiresMainQueueSetup
+{
+    return NO;
+}
+
+RCT_EXTERN_METHOD(list:(NSString)id withFailureCallback:(RCTResponseSenderBlock)failureCallback withSuccessCallback:(RCTResponseSenderBlock)successCallback)
+
+RCT_EXTERN_METHOD(upload:(NSString)id withData:(NSString)data withImage:(NSInteger)image withCallback:(RCTResponseSenderBlock)callback)
+
+RCT_EXTERN_METHOD(test:(NSString)id withHash:(NSString)hash withFailureCallback:(RCTResponseSenderBlock)failureCallback withSuccessCallback:(RCTResponseSenderBlock)successCallback)
+
+RCT_EXTERN_METHOD(reset:(NSString)id withFailureCallback:(RCTResponseSenderBlock)failureCallback withSuccessCallback:(RCTResponseSenderBlock)successCallback)
+
+RCT_EXTERN_METHOD(confirm:(NSString)id withHash:(NSString)hash withFailureCallback:(RCTResponseSenderBlock)failureCallback withSuccessCallback:(RCTResponseSenderBlock)successCallback)
 
 @end
